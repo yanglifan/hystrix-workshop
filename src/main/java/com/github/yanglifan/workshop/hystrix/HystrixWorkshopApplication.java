dc6761da2b316e012af0d3ccc8a6d0dfd7759844
Yang Lifan <yanglifan@gmail.com>
2017-08-25 16:47:55 +0800

    Enable Hystrix stream and Dashboard

--- src/main/java/com/github/yanglifan/workshop/hystrix/HystrixWorkshopApplication.java
+++ src/main/java/com/github/yanglifan/workshop/hystrix/HystrixWorkshopApplication.java
@@ -2,11 +2,15 @@ package com.github.yanglifan.workshop.hystrix;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
+@EnableHystrix
+@EnableHystrixDashboard
 @SpringBootApplication
 public class HystrixWorkshopApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HystrixWorkshopApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixWorkshopApplication.class, args);
+    }
 }
