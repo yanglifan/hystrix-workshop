6e43e121c90f94d732c44bfc3808d89e846833c8
Yang Lifan <yanglifan@gmail.com>
2018-01-10 20:46:19 +0800

    Demo fallback by the thread pool reject

--- src/main/java/com/github/yanglifan/workshop/hystrix/FallbackDemo.java
+++ src/main/java/com/github/yanglifan/workshop/hystrix/FallbackDemo.java
@@ -2,6 +2,7 @@ package com.github.yanglifan.workshop.hystrix;
 
 import com.netflix.hystrix.HystrixCommand;
 import com.netflix.hystrix.HystrixCommandGroupKey;
+import com.netflix.hystrix.HystrixThreadPoolProperties;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.is;
@@ -11,7 +12,10 @@ import static org.junit.Assert.assertThat;
  * @author Yang Lifan
  */
 public class FallbackDemo {
-    private static final HystrixCommandGroupKey GROUP = HystrixCommandGroupKey.Factory.asKey("FallbackDemo");
+    private static final HystrixCommandGroupKey GROUP =
+            HystrixCommandGroupKey.Factory.asKey("FallbackDemo");
+    private static final HystrixCommandGroupKey SINGLE_THREAD =
+            HystrixCommandGroupKey.Factory.asKey("FallbackDemo.SingleThread");
 
     /**
      * getFallback cannot be controlled by timeout configure.
@@ -21,6 +25,37 @@ public class FallbackDemo {
         FallbackTimeoutCommand command = new FallbackTimeoutCommand();
         String result = command.execute();
         assertThat(result, is("fallback"));
+    }
+
+    @Test
+    public void fallback_by_thread_pool_reject() {
+        SingleThreadPoolCommand command1 = new SingleThreadPoolCommand();
+        command1.queue();
+
+        SingleThreadPoolCommand command2 = new SingleThreadPoolCommand();
+        String result = command2.execute();
+        assertThat(result, is("fallback"));
+    }
+
+    class SingleThreadPoolCommand extends HystrixCommand<String> {
+        SingleThreadPoolCommand() {
+            super(Setter.withGroupKey(SINGLE_THREAD)
+                    .andThreadPoolPropertiesDefaults(
+                            HystrixThreadPoolProperties.Setter().withCoreSize(1)
+                    )
+            );
+        }
+
+        @Override
+        protected String run() throws Exception {
+            Thread.sleep(500);
+            return "hello";
+        }
+
+        @Override
+        protected String getFallback() {
+            return "fallback";
+        }
     }
 
     class FallbackTimeoutCommand extends HystrixCommand<String> {
